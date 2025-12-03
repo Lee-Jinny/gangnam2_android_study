b9c6a090749724529f3026ccf93d151b5075f08c
Jieun Lee <geniee1750@gmail.com>
2025-12-03 16:12:03 +0900

    [과제] chore: asset 준비

---
+++ app/src/main/java/com/survivalcoding/gangnam2kiandroidstudy/ui/theme/AppTextStyles.kt
@@ -0,0 +1,48 @@
+package com.survivalcoding.gangnam2kiandroidstudy.ui.theme
+
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.sp
+
+val titleTextBold = TextStyle(
+    fontFamily = AppFonts.Poppins,
+    fontWeight = FontWeight.Bold,
+    fontSize = 50.sp,
+    lineHeight = 75.sp
+)
+val headerTextBold = TextStyle(
+    fontFamily = AppFonts.Poppins,
+    fontWeight = FontWeight.Bold,
+    fontSize = 30.sp,
+    lineHeight = 45.sp
+)
+val largeTextBold = TextStyle(
+    fontFamily = AppFonts.Poppins,
+    fontWeight = FontWeight.Bold,
+    fontSize = 20.sp,
+    lineHeight = 30.sp
+)
+val mediumTextBold = TextStyle(
+    fontFamily = AppFonts.Poppins,
+    fontWeight = FontWeight.Bold,
+    fontSize = 20.sp,
+    lineHeight = 30.sp
+)
+val normalTextBold = TextStyle(
+    fontFamily = AppFonts.Poppins,
+    fontWeight = FontWeight.Bold,
+    fontSize = 16.sp,
+    lineHeight = 24.sp
+)
+val smallTextBold = TextStyle(
+    fontFamily = AppFonts.Poppins,
+    fontWeight = FontWeight.Bold,
+    fontSize = 14.sp,
+    lineHeight = 21.sp
+)
+val smallerTextBold = TextStyle(
+    fontFamily = AppFonts.Poppins,
+    fontWeight = FontWeight.Bold,
+    fontSize = 11.sp,
+    lineHeight = 17.sp
+)
