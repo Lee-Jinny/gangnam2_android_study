fc778d318efaec681693f0c3cec6b3b2e8ec0569
Jieun Lee <geniee1750@gmail.com>
2025-12-03 17:20:39 +0900

    [과제] feat: 연습문제 1 버튼 컴포넌트 작성

--- app/src/main/java/com/survivalcoding/gangnam2kiandroidstudy/MainActivity.kt
+++ app/src/main/java/com/survivalcoding/gangnam2kiandroidstudy/MainActivity.kt
@@ -4,44 +4,62 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.foundation.layout.size
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import com.survivalcoding.gangnam2kiandroidstudy.ui.theme.Gangnam2kiAndroidStudyTheme
+import androidx.compose.ui.unit.dp
+import com.survivalcoding.gangnam2kiandroidstudy.presentation.component.buttons.BigButton
+import com.survivalcoding.gangnam2kiandroidstudy.presentation.component.buttons.MediumButton
+import com.survivalcoding.gangnam2kiandroidstudy.presentation.component.buttons.SmallButton
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
-            Gangnam2kiAndroidStudyTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+            Column(
+                modifier = Modifier
+                    .fillMaxSize(),
+                horizontalAlignment = Alignment.CenterHorizontally,
+                verticalArrangement = Arrangement.Center
+            ) {
+                Column(
+                    modifier = Modifier.fillMaxSize(),
+                    horizontalAlignment = Alignment.CenterHorizontally,
+                    verticalArrangement = Arrangement.Center
+                ) {
+                    BigButton("Button", onClick = {
+                        println("BigButton 눌림!!!")
+                    })
+                    Spacer(modifier = Modifier.size(12.dp))
+
+                    BigButton("Disabled", enabled = false)
+                    Spacer(modifier = Modifier.size(12.dp))
+
+                    MediumButton("Button", onClick = {
+                        println("MediumButton 눌림!!!")
+                    })
+                    Spacer(modifier = Modifier.size(12.dp))
+
+                    MediumButton("Disabled", enabled = false)
+                    Spacer(modifier = Modifier.size(12.dp))
+
+                    SmallButton("Button", onClick = {
+                        println("SmallButton 눌림!!!")
+                    })
+                    Spacer(modifier = Modifier.size(12.dp))
+
+                    SmallButton("Disabled", enabled = false)
                 }
+
             }
+
+
         }
     }
 }
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    Gangnam2kiAndroidStudyTheme {
-        Greeting("Android")
-    }
-}
