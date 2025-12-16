19569b0c0ee960de453f62ba2b52150c23034f02
Jieun Lee <geniee1750@gmail.com>
2025-12-16 19:17:41 +0900

    chore: 직렬화 어노테이션

--- app/src/main/java/com/survivalcoding/gangnam2kiandroidstudy/data/dto/IngredientDto.kt
+++ app/src/main/java/com/survivalcoding/gangnam2kiandroidstudy/data/dto/IngredientDto.kt
@@ -1,5 +1,8 @@
 package com.survivalcoding.gangnam2kiandroidstudy.data.dto
 
+import kotlinx.serialization.Serializable
+
+@Serializable
 data class IngredientDto(
     val id: Int? = null,
     val name: String? = null,
--- app/src/main/java/com/survivalcoding/gangnam2kiandroidstudy/data/dto/ProcedureDto.kt
+++ app/src/main/java/com/survivalcoding/gangnam2kiandroidstudy/data/dto/ProcedureDto.kt
@@ -1,5 +1,8 @@
 package com.survivalcoding.gangnam2kiandroidstudy.data.dto
 
+import kotlinx.serialization.Serializable
+
+@Serializable
 data class ProcedureDto(
     val recipeId: Int? = null,
     val step: Int? = null,
