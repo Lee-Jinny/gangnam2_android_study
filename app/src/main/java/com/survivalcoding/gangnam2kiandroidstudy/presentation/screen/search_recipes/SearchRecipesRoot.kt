41609e8087edfe11909067fa48098656a7eff20c
Jieun Lee <geniee1750@gmail.com>
2025-12-18 18:59:06 +0900

    [과제] chore: 코드리뷰 반영하여 import 경로 수정

--- app/src/main/java/com/survivalcoding/gangnam2kiandroidstudy/presentation/screen/search_recipes/SearchRecipesRoot.kt
+++ app/src/main/java/com/survivalcoding/gangnam2kiandroidstudy/presentation/screen/search_recipes/SearchRecipesRoot.kt
@@ -8,7 +8,7 @@ import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
-import androidx.hilt.navigation.compose.hiltViewModel
+import androidx.hilt.lifecycle.viewmodel.compose.hiltViewModel
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import com.survivalcoding.gangnam2kiandroidstudy.R
 import com.survivalcoding.gangnam2kiandroidstudy.presentation.component.appbar.CustomAppTopBar
