69f3c422dd8d8c665e09d36d96d3e1a99dd528d2
Jieun Lee <geniee1750@gmail.com>
2025-12-18 18:55:54 +0900

    [과제] fix: 코드리뷰 반영하여 버전 이름 접미사 오류 수정

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -54,7 +54,7 @@ android {
         }
         create("qa") {
             dimension = "version"
-            versionName = "-qa"
+            versionNameSuffix = "-qa"
             applicationIdSuffix = ".qa"
         }
     }
